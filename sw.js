713c55a989a5eb6e637b6f146eb31854d96e0222
BorinelliConfeccoes <danpcompany@gmail.com>
2026-07-07 14:32:59 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'borinelli-prod-v4';
+const ARQUIVOS = [
+  './index.html',
+  './manifest.json',
+  './icone-192.png',
+  './logo.png',
+  './icone-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.8/html5-qrcode.min.js'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQUIVOS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const cp = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cp));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
